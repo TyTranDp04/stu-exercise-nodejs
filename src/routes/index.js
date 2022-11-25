70af2809ab0b6c79b02c96b1f1b41cb0b896db76
TyTranDp04 <ty.tran_dp04@devplus.edu.vn>
2022-11-25 09:38:18 +0700

    Implement content node ty/#concerns

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { DpConcernsController } from "../controllers/dpConcerns.js";
 
 const router = express.Router();
 
+router.get("/api/dpconcerns", DpConcernsController.get);
+router.post("/api/dpconcerns", DpConcernsController.create);
+router.patch("/api/dpconcerns/:_id", DpConcernsController.update);
+router.delete("/api/dpconcerns/:_id", DpConcernsController.delete);
+router.get("/api/dpconcerns/:_id", DpConcernsController.restore);
 
 export default router;
---
+++ src/schemas/index.js
@@ -0,0 +1,1 @@
+export { DpConcernsSchema } from "./dpConcerns";
